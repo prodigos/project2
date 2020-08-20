a19f6fdad8c7f5cb3f2f5399801bef29728156d8
prodigos <wilghensantos@gmail.com>
2020-08-19 23:37:58 -0400

    get res ok 200, working on grabbing response

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,11 @@
+let key = "gzd5QMerlYZRm97VihohsmpTccaZR2XO"
 $(".submit").click(() => {
-  console.log("clicked!");
+  let userInput = $(".search").val();
+  $.ajax({
+    url:`https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${userInput}=&limit=25&offset=0&rating=g&lang=en`,
+  }).done((res) => {
+      
+    console.log("res",res)
+  });
+
 });
