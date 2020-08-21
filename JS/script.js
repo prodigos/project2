29f1dc3a24fc74e0f94a02eb9200fbb1f17e5010
prodigos <wilghensantos@gmail.com>
2020-08-20 23:28:15 -0400

    able to get the gif and append it to div

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,15 @@
 let key = "gzd5QMerlYZRm97VihohsmpTccaZR2XO";
+
 $(".submit").click(() => {
   let userInput = $(".search").val();
+  let userLimit = $(".limit").val();
   $.ajax({
-    url: `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${userInput}=&limit=25&offset=0&rating=g&lang=en`,
+    url: `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${userInput}=&limit=${userLimit}&offset=0&rating=g&lang=en`,
   }).done((res) => {
     let gifsArray = res.data;
-    debugger;
     $.each(gifsArray, (i, e) => {
-      let postGifs = e.url;
-      $(".container").append(postGifs);
-      console.log(postGifs);
+        let postGifs = e.images.downsized.url;
+        $(".container").append(`<img src="${postGifs}"/>`);
     });
   });
 });
